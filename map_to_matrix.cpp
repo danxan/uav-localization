31ea807b4ec8c8da8c87e1391f10d794ac584f6a
root <root@localhost.localdomain>
2019-11-15 16:22:22 -0500

    Map to matrix file : reads a file and generates the patch indeces

--- map_to_matrix.cpp
+++ map_to_matrix.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <cstring>
+#include <fstream>
+#include <string>
 #include <stdio.h>
 #include <math.h>
+#include <sstream>
 using namespace std;
 
 #define MAP_WIDTH 4800
 #define MAP_HEIGHT 4800
-
-typedef struct _patch{
-  int instances;
-  string object;
-  struct _patch* suiv = NULL;
-} patch;
+#define N_CLASSES 60
 
 int width_conversion(int batch_width)  //batch size is in pixels
 {
@@ -63,20 +60,84 @@ int target_width = 13;
 int new_height = height_conversion(target_height);
 int new_width = width_conversion(target_width);
 
-patch** tab = new patch*[target_height];
-for( int i = 0; i < target_height; i++ ) {
-  patch* row = new patch[target_width];
+int*** tab = new int**[target_height];
+for( int i = 0; i < target_height; i++ )
+{
+  int** row = new int*[target_width];
+  for( int j = 0; j < N_CLASSES; j++ )
+  {
+    int* patch = new int[N_CLASSES];
+    row[j] = patch;
+  }
   tab[i] = row;
 }
 
 
 //read files here and getting the center coordinates x & y
+int x1, x2, y1, y2;
+int class_id = 0;
 
-int x = 0;
-int y = 0;
+fstream newfile;
+newfile.open("test.txt",ios::in); //open a file to perform read operation using file object
+if (newfile.is_open())
+{
+  string tp;
+  char* mot;
+  while(getline(newfile, tp)) //EVERY LINE
+  {
+    int i = 0;
+    int m = 0;
+    cout << "line: " << tp << '\n';
 
-new_x = ;
-new_y = ;
+    for (string::iterator it=tp.begin(); it!=tp.end(); ++it)
+    {
+      if(*it != ' ')
+      {
+        mot[i] = *it;
+        i++;
+      }
+
+      else
+      {
+        mot[i] = '\0';
+        std::istringstream iss (mot);
+        if(m == 0)
+        {
+          sscanf(mot, "%d", &x1);
+          cout << "x1: "<<x1<<"--";
+        }
+        if(m == 1)
+        {
+          sscanf(mot, "%d", &y1);
+          cout << "y1: "<<y1<<"--";
+        }
+        if(m == 2)
+        {
+          sscanf(mot, "%d", &x2);
+          cout << "x2: "<<x2<<"--";
+        }
+        if(m == 3)
+        {
+          sscanf(mot, "%d", &y2);
+          cout << "y2: "<<y2<<"--";
+        }
+        if(m == 4)
+        {
+          sscanf(mot, "%d", &class_id);
+          cout << "id: "<<class_id<<'\n'<<'-'<<'\n'<<'-'<<'\n';
+        }
+        i = 0;
+        m++;
+      }
+    }
+    int x = (x1 + x2) / 2;
+    int y = (y1 + y2) / 2;
+    int new_x = floor(x/new_height);
+    int new_y = floor(y/new_width);
+    cout << new_x <<':'<< new_y<<'\n';
+  }
+newfile.close(); //close the file object.
+}
 
 return 0;
 }
